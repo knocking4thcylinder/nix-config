be6efdb61b61ea95aae0c96a90dfd678492253d5
knocking4thcylinder <lev.fedorets@ya.ru>
2024-11-21 00:15:54 +0300

    .

--- home-manager/neovim/nvim/lua/plugins/chadtree.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "ms-jpq/chadtree",
-    config = function ()
-        vim.keymap.set("n", "<C-n>", ":CHADopen<CR>", {})
-    end
-}
--- home-manager/neovim/nvim/lua/plugins/colorscheme.lua
+++ home-manager/neovim/nvim/lua/plugins/colorscheme.lua
@@ -1,91 +1,77 @@
 return {
-	-- {
-	-- 	"cdmill/neomodern.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("neomodern").setup({
-	-- 			style = "roseprime",
-	-- transparent = true,
-	-- term_colors = false;
-	-- 		})
-	-- 		require("neomodern").load()
-	-- 	end,
-	-- },
-	{
-		"rose-pine/neovim",
-		config = function()
-			require("rose-pine").setup({
-				variant = "main", -- auto, main, moon, or dawn
-				dark_variant = "main", -- main, moon, or dawn
-				dim_inactive_windows = false,
-				extend_background_behind_borders = true,
 
-				enable = {
-					terminal = true,
-					legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
-					migrations = true, -- Handle deprecated options automatically
-				},
+    "rose-pine/neovim",
+    config = function()
+        require("rose-pine").setup({
+            variant = "main", -- auto, main, moon, or dawn
+            dark_variant = "main", -- main, moon, or dawn
+            dim_inactive_windows = false,
+            extend_background_behind_borders = true,
 
-				styles = {
-					bold = true,
-					italic = true,
-					transparency = true,
-				},
+            enable = {
+                terminal = true,
+                legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
+                migrations = true, -- Handle deprecated options automatically
+            },
 
-				groups = {
-					border = "muted",
-					link = "iris",
-					panel = "surface",
+            styles = {
+                bold = true,
+                italic = true,
+                transparency = true,
+            },
 
-					error = "love",
-					hint = "iris",
-					info = "foam",
-					note = "pine",
-					todo = "rose",
-					warn = "gold",
+            groups = {
+                border = "muted",
+                link = "iris",
+                panel = "surface",
 
-					git_add = "foam",
-					git_change = "rose",
-					git_delete = "love",
-					git_dirty = "rose",
-					git_ignore = "muted",
-					git_merge = "iris",
-					git_rename = "pine",
-					git_stage = "iris",
-					git_text = "rose",
-					git_untracked = "subtle",
+                error = "love",
+                hint = "iris",
+                info = "foam",
+                note = "pine",
+                todo = "rose",
+                warn = "gold",
 
-					h1 = "iris",
-					h2 = "foam",
-					h3 = "rose",
-					h4 = "gold",
-					h5 = "pine",
-					h6 = "foam",
-				},
+                git_add = "foam",
+                git_change = "rose",
+                git_delete = "love",
+                git_dirty = "rose",
+                git_ignore = "muted",
+                git_merge = "iris",
+                git_rename = "pine",
+                git_stage = "iris",
+                git_text = "rose",
+                git_untracked = "subtle",
 
-				highlight_groups = {
-					-- Comment = { fg = "foam" },
-					-- VertSplit = { fg = "muted", bg = "muted" },
-				},
+                h1 = "iris",
+                h2 = "foam",
+                h3 = "rose",
+                h4 = "gold",
+                h5 = "pine",
+                h6 = "foam",
+            },
 
-				before_highlight = function(group, highlight, palette)
-					-- Disable all undercurls
-					if highlight.undercurl then
-						highlight.undercurl = false
-					end
+            highlight_groups = {
+                -- Comment = { fg = "foam" },
+                -- VertSplit = { fg = "muted", bg = "muted" },
+            },
 
-					-- Change palette colour
-					if highlight.fg == palette.pine then
-						highlight.fg = palette.foam
-					end
-				end,
-			})
-			vim.cmd("colorscheme rose-pine")
-			-- vim.cmd("highlight Normal guibg=none")
-			-- vim.cmd("highlight NonText guibg=none")
-			-- vim.cmd("highlight Normal ctermbg=none")
-			-- vim.cmd("highlight NonText ctermbg=none")
-		end,
-	},
+            before_highlight = function(group, highlight, palette)
+                -- Disable all undercurls
+                if highlight.undercurl then
+                    highlight.undercurl = false
+                end
+
+                -- Change palette colour
+                if highlight.fg == palette.pine then
+                    highlight.fg = palette.foam
+                end
+            end,
+        })
+        vim.cmd("colorscheme rose-pine")
+        -- vim.cmd("highlight Normal guibg=none")
+        -- vim.cmd("highlight NonText guibg=none")
+        -- vim.cmd("highlight Normal ctermbg=none")
+        -- vim.cmd("highlight NonText ctermbg=none")
+    end,
 }
