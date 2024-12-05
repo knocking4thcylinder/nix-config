40e1e4ee60c9698ebae0e52b8a33a29ad8b50305
knocking4thcylinder <lev.fedorets@ya.ru>
2024-12-05 20:27:36 +0300

    changed handling neovim configuration

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -3,31 +3,22 @@
 {
   programs.neovim = {
     enable = true;
-    vimAlias = true;
-    viAlias = true;
   };
 
-  home = {
-    file.".config/nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink ./nvim;
-      recursive = true;
-    };
-
-    packages = with pkgs; [
-      haskell-language-server
-      stylua
-      luajit
-      lua-language-server
-      nixd
-      lemminx
-      java-language-server
-      rust-analyzer
-      zls
-      clang-tools
-      python312Packages.python-lsp-server
-      python312Packages.black
-      python312Packages.isort
-      nixfmt-rfc-style
-    ];
-  };
+  home.packages = with pkgs; [
+    haskell-language-server
+    stylua
+    luajit
+    lua-language-server
+    nixd
+    lemminx
+    java-language-server
+    rust-analyzer
+    zls
+    clang-tools
+    python312Packages.python-lsp-server
+    python312Packages.black
+    python312Packages.isort
+    nixfmt-rfc-style
+  ];
 }
--- home-manager/terminal/zsh.nix
+++ home-manager/terminal/zsh.nix
@@ -29,6 +29,8 @@
         rebuild = "nh os switch ~/nix-config";
         update = "nix flake update --flake ~/nix-config";
         ssh = "TERM=xterm-256color ssh";
+        vim = "nvim -u ~/nix-config/home-manager/neovim/nvim/init.lua";
+        nvim = "nvim -u ~/nix-config/home-manager/neovim/nvim/init.lua";
         # nix-shell = "nix-shell --run zsh";
       };
       history = {
