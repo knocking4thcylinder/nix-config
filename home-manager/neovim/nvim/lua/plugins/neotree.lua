898eb34d4c38dbbad9559aef8effd978bfdeb19d
knocking4thcylinder <lev.fedorets@ya.ru>
2024-12-10 01:21:18 +0300

    .

--- home-manager/neovim/nvim/lua/plugins/neotree.lua
+++ home-manager/neovim/nvim/lua/plugins/neotree.lua
@@ -1,44 +1,26 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	requires = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-tree/nvim-web-devicons",
-		{
-			"s1n7ax/nvim-window-picker",
-			version = "2.*",
-			config = function()
-				require("window-picker").setup({
-					filter_rules = {
-						include_current_win = false,
-						autoselect_one = true,
-						-- filter using buffer options
-						bo = {
-							-- if the file type is one of following, the window will be ignored
-							filetype = { "neo-tree", "neo-tree-popup", "notify" },
-							-- if the buffer type is one of following, the window will be ignored
-							buftype = { "terminal", "quickfix" },
-						},
-					},
-				})
-			end,
-		},
-	},
-	config = function()
-		require("neo-tree").setup({
-			close_if_last_window = true,
-			popup_border_style = "rounded",
-			default_component_config = {
-				indent = {
-					padding = 0,
-				},
-			},
-			window = {
-				position = "right",
-				width = 35,
-			},
-		})
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      popup_border_style = "rounded",
+      default_component_config = {
+        indent = {
+          padding = 0,
+        },
+      },
+      window = {
+        position = "right",
+        width = 35,
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal<CR>", {})
+  end,
 }
